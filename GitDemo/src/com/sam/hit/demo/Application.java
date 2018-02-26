1937d305672ef8ba94a1e74a10a5df6b366bd699
SAMUEL GDE <SAMUEL GDE@DESKTOP-H67R46U>
2018-02-25 18:59:46 -0600

    add printer

--- GitDemo/src/com/sam/hit/demo/Application.java
+++ GitDemo/src/com/sam/hit/demo/Application.java
@@ -2,8 +2,12 @@ package com.sam.hit.demo;
 
 public class Application {
 	public static void main(String args[]){
-		Employee emp= new Employee();
+		Employee emp= new Employee("001","sami", "engineer");
+		
 		System.out.println("hi GitHub. This is from eclipse idea good to meet you!");
+		System.out.println(" -------------------------");
+		System.out.println(" | " +emp.getId() +" | " + emp.getfName()+" | " +emp.getPosition() +" | " );
+		System.out.println(" -------------------------");
 	}
 
 }
--- GitDemo/src/com/sam/hit/demo/Employee.java
+++ GitDemo/src/com/sam/hit/demo/Employee.java
@@ -5,5 +5,31 @@ public class Employee {
 	String id;
 	String fName;
 	String position;
+
+	public Employee(String id, String fName, String position) {
+		super();
+		this.id = id;
+		this.fName = fName;
+		this.position = position;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
 	
 }
