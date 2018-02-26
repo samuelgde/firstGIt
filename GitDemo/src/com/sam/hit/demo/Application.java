5a167f777bfd9afb25ec165cad457f7741e1327b
SAMUEL GDE <SAMUEL GDE@DESKTOP-H67R46U>
2018-02-25 20:19:38 -0600

    update

--- GitDemo/src/com/sam/hit/demo/Application.java
+++ GitDemo/src/com/sam/hit/demo/Application.java
@@ -2,14 +2,14 @@ package com.sam.hit.demo;
 
 public class Application {
 	public static void main(String args[]){
-		Employee emp= new Employee("001","sami", "engineer");
+		Employee emp= new Employee("1" ,"sam" ,"eng");
 		
 		System.out.println("hi GitHub. This is from eclipse idea good to meet you!");
 		System.out.println();
 	        System.out.println("Find the details of the employee below");
-		System.out.println(" -------------------------");
+		System.out.println(" -----------------");
 		System.out.println(" | " +emp.getId() +" | " + emp.getfName()+" | " +emp.getPosition() +" | " );
-		System.out.println(" -------------------------");
+		System.out.println(" -----------------");
 		
 	}
 
