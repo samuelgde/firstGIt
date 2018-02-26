f94aacdc53072ba113f4a5c6b1f6139eb6a28562
SAMUEL GDE <SAMUEL GDE@DESKTOP-H67R46U>
2018-02-25 18:39:54 -0600

    First commit

---
+++ GitDemo/src/com/sam/hit/demo/Application.java
@@ -0,0 +1,8 @@
+package com.sam.hit.demo;
+
+public class Application {
+	public static void main(String args[]){
+		System.out.println("hi GitHub. This is from eclipse idea good to meet you!");
+	}
+
+}
---
+++ GitDemo/src/com/sam/hit/demo/Employee.java
@@ -0,0 +1,5 @@
+package com.sam.hit.demo;
+
+public class Employee {
+
+}
