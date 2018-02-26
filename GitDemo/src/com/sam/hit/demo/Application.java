972ff55fbe781221a8f0138fc3dd33bf719c9791
samuelgde <36828672+samuelgde@users.noreply.github.com>
2018-02-25 18:47:21 -0600

    Update Application.java
    creating an instance of employee

--- GitDemo/src/com/sam/hit/demo/Application.java
+++ GitDemo/src/com/sam/hit/demo/Application.java
@@ -2,6 +2,7 @@ package com.sam.hit.demo;
 
 public class Application {
 	public static void main(String args[]){
+		Employee emp= new Employee();
 		System.out.println("hi GitHub. This is from eclipse idea good to meet you!");
 	}
 
