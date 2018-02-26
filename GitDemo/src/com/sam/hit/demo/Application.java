0e32570c1e35979b5db01479d986eb442c3619d9
SAMUEL GDE <SAMUEL GDE@DESKTOP-H67R46U>
2018-02-25 19:08:13 -0600

    adding white space

--- GitDemo/src/com/sam/hit/demo/Application.java
+++ GitDemo/src/com/sam/hit/demo/Application.java
@@ -5,6 +5,7 @@ public class Application {
 		Employee emp= new Employee("001","sami", "engineer");
 		
 		System.out.println("hi GitHub. This is from eclipse idea good to meet you!");
+		System.out.println();
 			System.out.println("Find the details of the employee below");
 		System.out.println(" -------------------------");
 		System.out.println(" | " +emp.getId() +" | " + emp.getfName()+" | " +emp.getPosition() +" | " );
