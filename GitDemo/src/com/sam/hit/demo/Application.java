525cbbaa828d597ca2a173796414548c80e215f3
samuelgde <36828672+samuelgde@users.noreply.github.com>
2018-02-25 19:06:09 -0600

    Update Application.java

--- GitDemo/src/com/sam/hit/demo/Application.java
+++ GitDemo/src/com/sam/hit/demo/Application.java
@@ -5,9 +5,11 @@ public class Application {
 		Employee emp= new Employee("001","sami", "engineer");
 		
 		System.out.println("hi GitHub. This is from eclipse idea good to meet you!");
+			System.out.println("Find the details of the employee below");
 		System.out.println(" -------------------------");
 		System.out.println(" | " +emp.getId() +" | " + emp.getfName()+" | " +emp.getPosition() +" | " );
 		System.out.println(" -------------------------");
+		
 	}
 
 }
